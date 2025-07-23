0b98723742c4a69a405e2d3697e1b7b2a8f9dff5
Veries Seals <veries@me.com>
2025-07-22 21:31:24 -0500

    Created a married couple names class that takes in inputs using the scanner object

---
+++ src/married_couple_names.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+
+public class married_couple_names {
+    public static void main(String[] args) {
+    
+        // Input
+        // Create a Scanner object to read input from the console
+        // -------------------------------------------
+
+        Scanner scanner = new Scanner(System.in);
+        
+        // Define variables
+        // -------------------------------------------
+        String firstName1;
+        String lastName1;
+        String firstName2;
+        String lastName2;
+
+        // Get input from user
+        // -------------------------------------------
+        System.out.println("What is the first person's first name?");
+        firstName1 = scanner.nextLine();
+        System.out.print("What is the first person's last name?");
+        lastName1 = scanner.nextLine();
+        System.out.print("What is the second person's first name?");
+        firstName2 = scanner.nextLine();
+        System.out.print("What is the second person's last name?");
+        lastName2 = scanner.nextLine();
+
+        // Output
+        // -------------------------------------------
+        System.out.println("Here are some common married-couple names:");
+        System.out.println(firstName1 + " " + lastName1 + " and " +
+                            firstName2 + " " + lastName2);
+        System.out.println(firstName1 + " and " + firstName2 + " " + lastName1);
+        System.out.println(firstName1 + " and " + firstName2 + " " + lastName2);
+        // FIXME: Print two hyphenated last name options, with either last name
+        //        appearing first. (A hyphen can be written as "-")
+
+        System.out.println(firstName1 + " and " + firstName2 + " " + lastName1 + "-" + lastName2);
+        System.out.println(firstName1 + " and " + firstName2 + " " + lastName2 + "-" + lastName1);
+
+
+    }
+}
